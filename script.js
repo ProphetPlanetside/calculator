5e4ac01b842a2061b4ade75c374ca89a2582332e
Jay Spendlove <jayaloha@hotmail.com>
2022-06-10 12:36:04 -0700

    Added operate function.

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+// At this time, this function expects a string for the operator and will throw an error if a string isn't
+// provided in the function-call.
+function operate(operator, a, b) {
+    if (operator == '+')
+        return add(a, b);
+    if (operator == '-')
+        return subtract(a, b);
+    if (operator == '*')
+        return multiply(a, b);
+    if (operator == '/')
+        return divide(a, b);
+}
