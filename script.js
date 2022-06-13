d7bfd1ca476080c3ddd755c6da376bcaee8b6400
Jay Spendlove <jayaloha@hotmail.com>
2022-06-13 14:59:34 -0700

    Added initial functionality of all buttons in the calculator. Everything works, but there are bugs that need fixing and more editing to make it look nicer.

--- script.js
+++ script.js
@@ -20,6 +20,10 @@ function multiply(a, b) {
 function divide(a, b) {
     a = Number(a);
     b = Number(b);
+    if (b == 0) {
+        alert("You can't divide by zero!");
+        return;
+    }
     return a / b;
 }
 
@@ -36,37 +40,92 @@ function operate(operator, a, b) {
         return divide(a, b);
 }
 
-let total = '';
+function populateDisplay(a) {
+    displayValue += a;
+    display.textContent = displayValue;
+}
+
+// Stores the current operator (+, -, *, or /).
+// Stores the first (left-side value into storedValue), and makes the displayValue = 0.
+// Also clears the display.
+function pressedOperator (a) {
+    if(displayValue == '') {
+        alert("You need to input a number first.");
+        return;
+    }
+    currentOperator = a;
+    storedValue = displayValue;
+    displayValue = '';
+    display.textContent = '';
+}
+
+function equals () {
+    displayValue = operate(currentOperator, storedValue, displayValue);
+    display.textContent = displayValue;
+    storedValue = '';
+}
+
+function clear () {
+    displayValue = '';
+    storedValue = '';
+    currentOperator = '';
+    display.textContent = displayValue;
+}
+
+let displayValue = '';
+let storedValue = '';
+let currentOperator = '';
 const display = document.querySelector('#display');
 
 // All numbered buttons below will add to the total variable like a string. i.e. pressing 1, then 2, then 3, will
 // make total = '123'.
 const btnOne = document.querySelector('#one');
-btnOne.addEventListener("click", function() {total += '1';});
+btnOne.addEventListener("click", function() {populateDisplay('1');});
 
 const btnTwo = document.querySelector('#two');
-btnTwo.addEventListener("click", function() {total += '2';});
+btnTwo.addEventListener("click", function() {populateDisplay('2');});
 
 const btnThree = document.querySelector('#three');
-btnThree.addEventListener("click", function() {total += '3';});
+btnThree.addEventListener("click", function() {populateDisplay('3');});
 
 const btnFour = document.querySelector('#four');
-btnFour.addEventListener("click", function() {total += '4';});
+btnFour.addEventListener("click", function() {populateDisplay('4');});
 
 const btnFive = document.querySelector('#five');
-btnFive.addEventListener("click", function() {total += '5';});
+btnFive.addEventListener("click", function() {populateDisplay('5');});
 
 const btnSix = document.querySelector('#six');
-btnSix.addEventListener("click", function() {total += '6';});
+btnSix.addEventListener("click", function() {populateDisplay('6');});
 
 const btnSeven = document.querySelector('#seven');
-btnSeven.addEventListener("click", function() {total += '7';});
+btnSeven.addEventListener("click", function() {populateDisplay('7');});
 
 const btnEight = document.querySelector('#eight');
-btnEight.addEventListener("click", function() {total += '8';});
+btnEight.addEventListener("click", function() {populateDisplay('8');});
 
 const btnNine = document.querySelector('#nine');
-btnNine.addEventListener("click", function() {total += '9';});
+btnNine.addEventListener("click", function() {populateDisplay('9');});
 
 const btnZero = document.querySelector('#zero');
-btnZero.addEventListener("click", function() {total += '0';});
+btnZero.addEventListener("click", function() {populateDisplay('0');});
+
+// Operator buttons
+const btnPlus = document.querySelector('#plus');
+btnPlus.addEventListener("click", function() {pressedOperator('+');});
+
+const btnMinus = document.querySelector('#minus');
+btnMinus.addEventListener("click", function() {pressedOperator('-');});
+
+const btnMultiply = document.querySelector('#multiply');
+btnMultiply.addEventListener("click", function() {pressedOperator('*');});
+
+const btnDivide = document.querySelector('#divide');
+btnDivide.addEventListener("click", function() {pressedOperator('/');});
+
+// Equals button
+const btnEquals = document.querySelector('#equals');
+btnEquals.addEventListener("click", function() {equals();});
+
+// Clear button
+const btnClear = document.querySelector('#clear');
+btnClear.addEventListener("click", function() {clear();});
